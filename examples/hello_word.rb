036cf38a488007c28ebb68fbcd2791de36767cae
Gerardo <centrodph@gmail.com>
2019-03-11 17:32:44 -0300

    function

--- examples/hello_word.rb
+++ examples/hello_word.rb
@@ -5,4 +5,15 @@ puts "Hello world" # return nil
 
 p "Hello world" # return "Hello world"
 
-print "Hello world" # no new line
+print "Hello world\n" # no new line
+
+
+greeting = "Hello world var"
+
+puts greeting
+
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello "say Hello"
