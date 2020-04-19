0667bdae441e56e96044918c49351037b7660271
Gerardo Perrucci <centrodph@gmail.com>
2020-04-18 21:36:24 -0300

    methods

--- examples/string.rb
+++ examples/string.rb
@@ -2,9 +2,18 @@ first_name = "Gerardo"
 last_name = "Perrucci"
 puts first_name + " " + last_name
 
-## string interpolation
+# string interpolation
 puts "my first name is #{first_name}!"
 
 full_name = "#{first_name} #{last_name}"
 
 puts "typeof full_name = #{full_name.class}"
+
+
+# puts full_name.methods
+
+
+
+puts full_name.nil?
+
+puts full_name.split("").join("_").split("_").join("")
