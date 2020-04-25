dbfe518d8731c8891852a6d40033f225bb245508
Gerardo Perrucci <centrodph@gmail.com>
2020-04-25 14:14:43 -0300

    compatison

---
+++ examples/comparison.rb
@@ -0,0 +1,17 @@
+# == equal
+# = assigns
+# !=
+# >
+# <
+
+puts 10 == 9
+puts 10 == "10".to_i
+puts 10 == 10.to_f
+
+puts "hello" != "bye"
+
+
+puts "10 === 10.to_f #{10 === 10.to_f}"
+
+# types
+puts "10.eq?(10.) #{10.eq?(10.)}"
