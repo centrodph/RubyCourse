920203699662ffa61377553c10d1a7bde38e65ca
Gerardo Perrucci <gerardo.perrucci@leniolabs.com>
2020-05-27 22:25:31 -0300

    hashes iterate, select, delete

--- examples/hashes.rb
+++ examples/hashes.rb
@@ -31,3 +31,23 @@ other_symbols.each do |key, value|
     puts "key: #{key}/#{key.class} value: #{value}/#{value.class}"
 end
 
+
+# add elment
+other_symbols[:added_key] = 'ruby'
+p other_symbols
+
+# change value
+other_symbols[:my_symbol] = 234
+p other_symbols
+
+
+# one line 
+other_symbols.each { |some_key, some_value| puts "#{some_key} : #{some_value}"}
+
+
+# select
+puts other_symbols.select { |k, v| v.is_a?(String)}
+
+# clean
+
+puts other_symbols.each { |k, v| other_symbols.delete(k) if v.is_a?(String)}
