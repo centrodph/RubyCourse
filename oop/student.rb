b88da2e292a1a4a92f99335439b7682856a3621c
Gerardo Perrucci <centrodph@gmail.com>
2020-06-13 19:34:18 -0300

    attr_accessor attr_reader attr_writer

--- oop/student.rb
+++ oop/student.rb
@@ -1,9 +1,22 @@
 class Student
+	# read more https://www.rubyguides.com/2018/11/attr_accessor/
+	attr_accessor :first_name, :last_name, :email
+	attr_reader :username
+
 	@first_name
 	@last_name
 	@email
 	@username
 	@password
+
+	# initialize
+	def initialize(first_name, last_name, username, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+	end
 
 	# default to string
 	def to_s
@@ -11,22 +24,27 @@ class Student
 	end
 
 	# setter method
-	def first_name=(name)
-		@first_name = name
-	end
+	# def first_name=(name)
+	# 	@first_name = name
+	# end
+
 	# getter method
-	def first_name
-		@first_name
-	end
+	# def first_name
+	# 	@first_name
+	# end
 
 end
 
-example = Student.new
+# example = Student.new
+# example.first_name = "Gerardo"
 
-example.first_name = "Gerardo"
+# # print full instance
+# puts example
 
-# print full instance
-puts example
+# # print just the name
+# puts example.first_name
 
-# print just the name
-puts example.first_name
+# other student
+ex2 = Student.new("Ger", "Per", "demo", "demo@gmail.com", "test123")
+
+puts ex2
