1f2b9d74ca25c1321888a0f1230a53c64cc70d3b
Gerardo Perrucci <centrodph@gmail.com>
2020-06-27 20:29:23 -0300

    feat: use self notation and add module doc

--- projects/crud.rb
+++ projects/crud.rb
@@ -1,27 +1,28 @@
 # frozen_string_literal: true
 
+# Crud module. This module allow you encrypt and authenticate users using BCrypt
 module Crud
   require 'bcrypt'
   puts 'Module CRUD activated'
 
-  def Crud.create_hash_digest(password)
+  def self.create_hash_digest(password)
     BCrypt::Password.create(password)
   end
 
-  def Crud.verify_hash_digest(password)
+  def self.verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
 
-  def Crud.secure_users_password(users)
+  def self.secure_users_password(users)
     users.each do |model|
-      model[:password] = Crud.create_hash_digest(model[:password])
+      model[:password] = create_hash_digest(model[:password])
     end
     users
   end
 
-  def Crud.authenticate_user(username, password, list_of_users)
+  def self.authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
-      return user_record if user_record[:username] == username && Crud.verify_hash_digest(user_record[:password]) == password
+      return user_record if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
     end
     'Credentials were not correct'
   end
