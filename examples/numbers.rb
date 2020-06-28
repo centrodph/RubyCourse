34e4910b70206829fd3750508b3c2e3a68f32120
Gerardo Perrucci <centrodph@gmail.com>
2020-06-27 21:09:34 -0300

    fix: format number

--- examples/numbers.rb
+++ examples/numbers.rb
@@ -1,26 +1,25 @@
-puts 1 + 2
+# frozen_string_literal: true
 
+puts 1 + 2
 
 puts 10 / 4
 
 puts 10.to_f / 4
 
-puts rand()
+puts rand
 
 puts rand(10)
 
 puts rand(1000)
 
-
 20.times { puts rand(100) }
 
-
-30.times { print "-" }
+30.times { print '-' }
 puts
-puts "Enter firt number"
+puts 'Enter firt number'
 first_number = gets.chomp
-puts "Enter second number"
+puts 'Enter second number'
 second_number = gets.chomp
-30.times { print "-" }
+30.times { print '-' }
 puts
 puts "The multiplication is : #{first_number.to_i * second_number.to_i}"
